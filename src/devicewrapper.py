abd290a89502ae595d8419651624eccb1327d9db
kehaus <tv_junkie_4ever@hotmail.com>
2020-03-30 19:12:34 -0400

    basic functionality of worker thread established

--- src/devicewrapper.py
+++ src/devicewrapper.py
@@ -5,7 +5,6 @@ Created on Sun Mar  8 23:42:27 2020
 
 
 TODO:
-    * signal back to DeviceWrapper from WorkerThread when thread is terminated
 
 """
 
@@ -14,9 +13,10 @@ TODO:
 import os
 import threading
 import queue
+import time
 
 
-from worker_thread import WorkerThread, WorkerTask
+from workerthread import WorkerThread, WorkerTaskBase
 
 
 
@@ -39,52 +39,83 @@ class DeviceWrapper():
     while device is part of a worker thread in the background.
     
     
+    Example:
+        >>> d = SomeDevice()
+        >>> d.some_function()
+        5
+        >>> dw = DeviceWrapper(d)
+        >>> dw.some_function()
+        5
+        
+        
     """
     
     def __init__(self, device):
-        self.d = device
-        self.thread_runs = False
-        
+        self.d = device        
         self._create_worker_thread()
         
     def __getattr__(self, name):
+        """redirects method and attribute calls to device object
+        
+        
+        """
         if hasattr(self.d, name):
-            if self.thread_runs:
-                self.func_name = name
-                return self.send_worker_task
+            if self.thread_is_alive():
+                self.func_name = getattr(self.d, name)
+                return self._send_worker_task
             else:
                 return getattr(self.d, name)
         else:
             raise AttributeError
         
     def _create_worker_thread(self):
+        """initialzes the workerthread """
         q = queue.Queue()
         self.wt = WorkerThread(q)
         
     def start(self):
-        """starts thread"""
-        if not self.thread_runs:
+        """starts workerthread"""
+        if not self.thread_is_alive():
             self.wt.start()
-            self.thread_runs = True
         else:
             raise DeviceWrapperException('Thread runs already.')
         
     def stop(self):
-        """stops thread"""
-        if self.thread_runs:
+        """stops workerthread"""
+        if self.thread_is_alive():
             self.wt.stop()
-            self.thread_runs = False
         else:
             raise DeviceWrapperException('Thread is not running.')
+    
+    def thread_is_alive(self):
+        return self.wt.is_alive()
         
-    def send_worker_task(self, *args, **kwargs):
-        w = WorkerTask(
+    def _send_worker_task(self, *args, **kwargs):
+        w = WorkerTaskBase(
             func=self.func_name, 
             args=args, 
-            kwargs=kwargs
+            kwargs=kwargs,
+            callback=self._callback
         )
         self.wt.put(w)
+        self.wait_for_callback = True
         
+        while self.wait_for_callback:
+            time.sleep(0.1)
+        return self.rtn
+        
+            
+    def _callback(self, return_args):
+        """helper function to retrieve return value from WorkTask object
+        
+        This function is part of the callback mechanism to retrieve the return
+        value from the WorkerTask object function call initiated by the _
+        send_worker_task function
+        
+        """
+        self.rtn = return_args
+        self.wait_for_callback = False
+
 
 
 
@@ -117,7 +148,9 @@ class C():
 if __name__ == "__main__":
     c = C()
     
-    wd = DeviceWrapper(c)
+    dw = DeviceWrapper(c)
+    dw.wt.q.put(WorkerTaskBase(dw.d.count, continuous=True))
+#    dw.start()
     
     pass
        
--- src/workerthread.py
+++ src/workerthread.py
@@ -9,11 +9,11 @@
 
 Example:
 
->>> wt = WorkerTask(func1)	#func1 needs to be defined already
+>>> wt = WorkerTask(func1)    #func1 needs to be defined already
 >>> q = queue.Queue()
->>> q.put(wt)				# load task into queue
+>>> q.put(wt)                # load task into queue
 >>> w(q)
->>> w.start()				# start thread
+>>> w.start()                # start thread
 
 """
 
@@ -24,21 +24,18 @@ import csv
 import time
 import inspect
 
-#from ..util.general_util import DataSet, TimeFormatter, CSVHandler, JSONHandler
-from general_util import DataSet, TimeFormatter, CSVHandler, JSONHandler
-
 
 
 # ===========================================================================
 # Exception class
 # ===========================================================================        
 class WorkerThreadException(Exception):
-	""" """
-	pass
+    """ """
+    pass
 
 class WorkerTaskException(Exception):
-	""" """
-	pass
+    """ """
+    pass
 
 
 # ===========================================================================
@@ -46,53 +43,56 @@ class WorkerTaskException(Exception):
 # ===========================================================================        
 
 class WorkerThread(threading.Thread):
-	"""
+    """creates a thread to 
 
 
 
-	Example:
-		>>> w = WorkerTask(func1, save=True, continuous=True)
-		>>> q = queue.Queue()
-		>>> q.put(w)
-		>>> ww = WorkerThread(q)
-		>>> ww.start()
+    Example:
+        >>> w = WorkerTask(func1, save=True, continuous=True)
+        >>> q = queue.Queue()
+        >>> q.put(w)
+        >>> ww = WorkerThread(q)
+        >>> ww.start()
 
-	To stop:
-		>>> ww.stop()
+    To stop:
+        >>> ww.stop()
 
-	 """
+     """
 
-	SLEEP_TIME = 0.1
+    SLEEP_TIME = 0.1
 
-	def __init__(self, q):
-		super(WorkerThread, self).__init__()
-		self.q = q
-		self._stop = True
-		self.sleep_time = WorkerThread.SLEEP_TIME
-
-
-	def run(self):
-
-		self._stop = False
-		while not self._stop:
-			if not self.q.empty():	
-				self.process_task()
-			time.sleep(self.sleep_time)
-
-	def stop(self):
-		self._stop = True
-
-	def put(self, w):
-		if not isinstance(w, WorkerTask):
-			raise WorkerThreadException('argument needs to be WorkerTask object')
-		else:
-			self.q.put(w)
+    def __init__(self, q=None):
+        super(WorkerThread, self).__init__()
+        if q is None: 
+            q=queue.Queue()
+        self.q = q
+        self._stop_thread = True
+        self.sleep_time = WorkerThread.SLEEP_TIME
 
 
-	def process_task(self):
-		task = self.q.get()
-		if task.continuous: self.q.put(task)
-		task.do_task()
+    def run(self):
+
+        self._stop_thread = False
+        while not self._stop_thread:
+            if not self.q.empty():    
+                self.process_task()
+            time.sleep(self.sleep_time)
+
+    def stop(self):
+        self._stop_thread = True
+
+    def put(self, w):
+        if not isinstance(w, WorkerTaskBase):
+            raise WorkerThreadException('argument needs to be WorkerTask object: {}'.format(w))
+        else:
+            self.q.put(w)
+
+
+    def process_task(self):
+        """removes task from queue and processes it"""
+        task = self.q.get()
+        if task.continuous: self.q.put(task)
+        task.do_task()
 
 
 # ===========================================================================
@@ -100,98 +100,157 @@ class WorkerThread(threading.Thread):
 # ===========================================================================        
 
 class WorkerTaskBase(object):
-	""" """
-	def __init__(self, obj, attr):
-		""" """
-		self.continuous = False
-		self.args = []
-		self.kwargs = {}
-		self.obj = obj
-		self.obj_attr = attr
-#		print('callable: ', callable(getattr(obj, attr)))
-#		print('_check:', self._check_func(getattr(obj, attr)))
-		self.obj_attr = self._check_func(getattr(obj, attr))
+    """ 
+    Represents a function call which is handed to the WorkerThread
+    
+    
+    
+    Example
+    >>> def func():
+    >>>     print('hello')
+    >>>
+    >>> wt = WorkerTaskBase(func, continuous=True)
+    
+    
+    """
+    COUNT = 0
+
+    def __init__(self, 
+                 func, 
+                 args=None, 
+                 kwargs=None, 
+                 continuous=False,
+                 callback=None,
+                 ):
+        WorkerTaskBase.COUNT += 1
+        self.continuous = continuous
+
+        self.func = self._check_func(func)
+        self.args = self._check_args(args)
+        self.kwargs = self._check_kwargs(kwargs)
+        self.callback = self._check_callback(callback)
+
+    def _check_func(self, func):
+        if callable(func) != True:
+            raise WorkerTaskException('func needs to be callable object: {}'.format(func))
+        return func
+    
+    def _check_callback(self, callback):
+        if callback is None:
+            return callback
+        if callable(callback) != True:
+            raise WorkerTaskException('callback needs to be callable object: {}'.format(callback))
+        return callback
+    
+
+    def _check_args(self, args):
+        """verify if args is list-like by checking if it is iterable"""
+        if args == None:
+            args = []
+        try:
+            for i in args:
+                pass
+        except TypeError:
+            raise WorkerTaskException('args is not a list-like object: {}'.format(args))
+        return args
+
+    def _check_kwargs(self, kwargs):
+        if kwargs == None:
+            kwargs = {}
+        if type(kwargs) != dict:
+            raise WorkerTaskException('kwargs needs to be of type dict: {}'.format(kwargs))
+        return kwargs
 
 
-		
+    def do_task(self):
+
+        self.rtn = self.func(*self.args, **self.kwargs)
+        if self.callback is not None: 
+            self.callback(self.rtn)
+#        return self.rtn
+    
+
+# ===========================================================================
+# Depricated
+# ===========================================================================        
+
+# class WorkerTask(DataSet):
+#     """ """
+
+#     COUNT = 0
+# #    DEFAULT_FILE_HEADER = [
+# #        'time',
+# #        'variable'
+# #    ]
+
+#     def __init__(self, 
+#                  func, 
+#                  args=None, 
+#                  kwargs=None, 
+#                  continuous=False, 
+#                  save=False, 
+#                  plot=False,
+#                  base_name=None):
+#         super().__init__(base_name=base_name)
+#         WorkerTask.COUNT += 1
+#         self.continuous = continuous
+        # self.continuous = self._check_continous_flag
+#         self.save = bool(save)
+#         self.fn_base = 'workertask{:d}'.format(WorkerTask.COUNT)
+#         self.plot = plot
+
+#         self.func = self._check_func(func)
+#         self.args = self._check_args(args)
+#         self.kwargs = self._check_kwargs(kwargs)
+
+#     def _check_func(self, func):
+#         if callable(func) != True:
+#             raise WorkerTaskException('func needs to be callable object')
+#         return func
+
+#     def _check_args(self, args):
+#         if args == None:
+#             args = []
+#         if type(args) != list:
+#             raise WorkerTaskException('args needs to be of type list')
+#         return args
+
+#     def _check_kwargs(self, kwargs):
+#         if kwargs == None:
+#             kwargs = {}
+#         if type(kwargs) != dict:
+#             raise WorkerTaskException('kwargs needs to be of type dict')
+#         return kwargs
 
 
+#     def plot_data(self):
+#         pass
 
-	def _check_func(self, func):
-		if callable(func) != True:
-			raise WorkerTaskException('func needs to be a callable object')
-		return func
+#     def do_task(self):
 
-	def load_args_kwargs(self, *args, **kwargs):
-		self.args = args
-		self.kwargs = kwargs
+#         self.rtn = self.func(*self.args, **self.kwargs)
 
-	def do_task(self):
-		rtn = self.obj_attr(*self.args, **self.kwargs)
-		return rtn
+#         if self.save != False:
+#             self.save_data_point(self.rtn)
 
+#         if self.plot != False:
+#             self.plot_data()
+#         return
 
-class WorkerTask(DataSet):
-	""" """
+    # def _check_continuous_flag(self, continuous):
+    #     if continuous is True or continuous is False:
+    #         return continuous
+    #     if continuous is None:
+    #         return False
+        
+    #     try:
+    #         if float(continuous).is_integer():
+    #             return float(continuous)
+    #         else:
+    #             raise WorkerTaskException('continous needs to be a boolean or an integer')
+    #     except ValueError:
+    #         raise WorkerTaskException('continous flag needs to be a boolean or an integer')
 
-	COUNT = 0
-#	DEFAULT_FILE_HEADER = [
-#		'time',
-#		'variable'
-#	]
-
-	def __init__(self, 
-				 func, 
-				 args=None, 
-				 kwargs=None, 
-				 continuous=False, 
-				 save=False, 
-				 plot=False,
-                 base_name=None):
-		super().__init__(base_name=base_name)
-		WorkerTask.COUNT += 1
-		self.continuous = continuous
-		self.save = bool(save)
-		self.fn_base = 'workertask{:d}'.format(WorkerTask.COUNT)
-		self.plot = plot
-
-		self.func = self._check_func(func)
-		self.args = self._check_args(args)
-		self.kwargs = self._check_kwargs(kwargs)
-
-	def _check_func(self, func):
-		if callable(func) != True:
-			raise WorkerTaskException('func needs to be callable object')
-		return func
-
-	def _check_args(self, args):
-		if args == None:
-			args = []
-		if type(args) != list:
-			raise WorkerTaskException('args needs to be of type list')
-		return args
-
-	def _check_kwargs(self, kwargs):
-		if kwargs == None:
-			kwargs = {}
-		if type(kwargs) != dict:
-			raise WorkerTaskException('kwargs needs to be of type dict')
-		return kwargs
-
-
-	def plot_data(self):
-		pass
-
-	def do_task(self):
-
-		self.rtn = self.func(*self.args, **self.kwargs)
-
-		if self.save != False:
-			self.save_data_point(self.rtn)
-
-		if self.plot != False:
-			self.plot_data()
-		return
 
 
 ### Dataset class
@@ -200,26 +259,34 @@ class WorkerTask(DataSet):
 
 
 
-### Examples usage case
+
+# ===========================================================================
+# Test function
+# ===========================================================================        
 
 def func1():
-	return 'func1'
+    return 'func1'
 
 def func2(x,y, z=8):
-	print('func2')
-	print('x: ', x, 'y: ', y, 'z: ', z)
-	return
+    print('func2')
+    print('x: ', x, 'y: ', y, 'z: ', z)
+    return
 
 def func3(v=33, w=55, z=9):
-	print('func3')
-	print('v: ', v, 'w: ', w, 'z: ',z)
-	return
+    print('func3')
+    print('v: ', v, 'w: ', w, 'z: ',z)
+    return
 
 
+# ===========================================================================
+# main function
+# ===========================================================================        
 if __name__ == "__main__":
-    wt = WorkerTask(func1, continuous=True)
+    wt = WorkerTaskBase(func3, continuous=True)
     q = queue.Queue()
+    q.put(wt)
     w = WorkerThread(q)
+#    w.start()
 
 
 
