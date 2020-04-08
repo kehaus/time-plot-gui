8f3a51cd3bb1bd79fe93b8833b414d89f7ca9193
= <=>
2020-04-08 15:31:00 -0400

    Graph updates continuously over time

--- src/time_plot_gui.py
+++ src/time_plot_gui.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-TimePlotGui class creates a GUI displaying the time trace of a value from a 
+TimePlotGui class creates a GUI displaying the time trace of a value from a
 given device object
 
 
@@ -28,6 +28,7 @@ from PyQt5.QtWidgets import qApp, QAction, QMenu, QGridLayout, QLabel
 from PyQt5.QtGui import QIcon, QFont, QCursor
 from PyQt5 import QtCore
 import pyqtgraph as pg
+import time
 
 
 from time_plot_worker import TimePlotWorker
@@ -48,39 +49,46 @@ class TimePlotGuiException(Exception):
 # ============================================================================
 
 class TimePlotGui(QWidget):
-    
+
+    ABSOLUTE_TIME = []
+    TIME = []
+    POTENTIAL = []
+
     start_signal = QtCore.pyqtSignal()
     stop_signal = QtCore.pyqtSignal()
-    
+
     def __init__(self, parent=None, window=None, devicewrapper=None):
         """ """
         super(TimePlotGui, self).__init__(parent=parent)
         self._init_ui(window)
         self._init_worker_thread(devicewrapper)
-        
-    
+
+
     def _init_ui(self, mainwindow):
 
         self.central_wid = QWidget(mainwindow)
         self._set_central_wid_properties()
         self.mainwindow = mainwindow
-        self.mainwindow.setCentralWidget(self.central_wid)  
-        
-        
+        self.mainwindow.setCentralWidget(self.central_wid)
+
+
 
         # grid layout to put all widgets
         self.wid_layout = QGridLayout()
-           
-        
+
+
         # =====================================================================
         # control panel
         # =====================================================================
         self.graphics_layout = QGridLayout()
         self.vl = pg.ValueLabel(formatStr='{avgValue:0.2f} {suffix}')
-        self.vl.setValue(-1)
-        self.graphics_layout.addWidget(self.vl, 0, 0, 4, 4)
-        
-        
+        if len(self.POTENTIAL) == 0:
+            self.vl.setValue(-1)
+        else:
+            self.vl.setValue(self.POTENTIAL[-1])
+        self.graphics_layout.addWidget(self.vl, 0, 0, 1, 1)
+
+
         # =====================================================================
         # control panel
         # =====================================================================
@@ -94,8 +102,23 @@ class TimePlotGui(QWidget):
         self.controls_layout.addWidget(self.startBtn, 0, 0, 1, 1)
         self.stopBtn = QPushButton('STOP')
         self.controls_layout.addWidget(self.stopBtn, 1, 0, 1, 1)
-        
-        
+        self.controls_layout.addWidget(self.vl, 0, 1, 1, 1)
+        #self.comboBox = QComboBox(self)
+        #comboBox.addItem()
+        #self.controls_layout.addWidget(self.comboBox, 0, 1, 1, 1)
+
+
+        #self.setCentralWidget(self.graphWidget)
+        # hour = [1,2,3,4,5,6,7,8,9,10]
+        # temperature = [30,32,34,32,33,31,29,32,35,45]
+        # temperature2 = [40, 20, 40, 40, 20, 20, 40, 30, 20, 30]
+        self.graphWidget = pg.PlotWidget()
+        self.graphics_layout.addWidget(self.graphWidget, 0, 3, 5, 5)
+        potential_axis = self.POTENTIAL
+        time_axis = self.TIME
+        self.graphWidget.plot(time_axis, potential_axis)
+
+
         # =====================================================================
         # control buttons - connections
         # =====================================================================
@@ -105,11 +128,11 @@ class TimePlotGui(QWidget):
        # ============================================================
         # put everything together
         # ============================================================
-        self.wid_layout.addItem(self.graphics_layout, 0, 0, 2, 2)
-        self.wid_layout.addItem(self.controls_layout, 0, 2, 2, 2)     
+        self.wid_layout.addItem(self.graphics_layout, 0, 4, 6, 6)
+        self.wid_layout.addItem(self.controls_layout, 0, 0, 2, 2)
         # self.wid_layout.setColumnStretch(0, 10)
         # self.wid_layout.setColumnStretch(8, 2)
-        
+
         self.central_wid.setLayout(self.wid_layout)
 
 
@@ -118,96 +141,103 @@ class TimePlotGui(QWidget):
         self.central_wid.setAutoFillBackground(True)
         p = self.central_wid.palette()
         p.setColor(self.central_wid.backgroundRole(), QtCore.Qt.darkGray)
-        self.central_wid.setPalette(p)    
-        
-  
+        self.central_wid.setPalette(p)
+
+
     def _init_worker_thread(self, devicewrapper):
         """ """
-        
+
         # Setup QWaitCondition
         self.mutex = QMutex()
         self.cond = QWaitCondition()
-        
-        # Setup the measurement engine        
+
+        # Setup the measurement engine
 #        self.mthread = QtCore.QThread()
-        self.worker = TimePlotWorker(devicewrapper, self.mutex, self.cond)        
-        
-        
+        self.worker = TimePlotWorker(devicewrapper, self.mutex, self.cond)
+
+
         # connect signal and slots
         self.start_signal.connect(self.worker.start)
         self.stop_signal.connect(self.worker.stop)
-        
-        
+
+
         self.worker.reading.connect(self.newReading)
-        
+
         return
 
 
     def start_thread(self):
         self.start_signal.emit()
-        
+
     def stop_thread(self):
         self.stop_signal.emit()
-        
+
+    # TODO allow graph to update
     def update_ValueLabel(self, val):
         """ """
         self.vl.setValue(val)
-        
+        self.POTENTIAL.append(val)
+
+        self.ABSOLUTE_TIME.append(time.time())
+        self.TIME = [x - self.ABSOLUTE_TIME[0] for x in self.ABSOLUTE_TIME]
+        self._init_ui(self.mainwindow)
+        #TIME.append(val)
+        #print(f"POTENTIAL: {self.POTENTIAL} \n TIME: {self.TIME}")
+
 
 
     @QtCore.pyqtSlot(float)
     def newReading(self, val):
         """ """
         pg.QtGui.QApplication.processEvents()
-        
         self.update_ValueLabel(val)
         time.sleep(0.1)         # necessary to avoid worker to freeze
-        self.cond.wakeAll()     # wake worker thread up 
+        self.cond.wakeAll()     # wake worker thread up
         return
 
 
 
     def closeEvent(self, event):
         reply = QMessageBox.question(self, 'Message',
-            "Are you sure to quit?", QMessageBox.Yes | 
+            "Are you sure to quit?", QMessageBox.Yes |
             QMessageBox.No, QMessageBox.Yes)
 
         if reply == QMessageBox.Yes:
             event.accept()
         else:
-            event.ignore()  
+            event.ignore()
 
 
 
 
 # ===========================================================================
-# 
+#
 # ===========================================================================
 
 class MainWindow(QMainWindow):
     """ """
-    
-    DEFAULT_GEOMETRY = [400, 400, 200, 50]
-    
+    # xpos on screen, ypos on screen, width, height
+    DEFAULT_GEOMETRY = [400, 400, 1000, 500]
+
     def __init__(self, devicewrapper=None):
         super(MainWindow, self).__init__()
         self._init_ui(devicewrapper=devicewrapper)
-        
+
     def _init_ui(self, window_geometry=None, devicewrapper=None):
         self.setGeometry()
         self.setWindowTitle('time-plot')
         self.time_plot_ui = TimePlotGui(
-            parent=None, 
-            window=self, 
+            parent=None,
+            window=self,
             devicewrapper=devicewrapper
         )
-        
+
     def setGeometry(self, *args, **kwargs):
         """ """
         if len(args) == 0 and len(kwargs) == 0:
             args = MainWindow.DEFAULT_GEOMETRY
         super(MainWindow, self).setGeometry(*args, **kwargs)
-        
+
     def closeEvent(self, event):
         """ """
         print('event')
@@ -226,17 +256,17 @@ def main(devicewrapper):
     else:
         print('QApplication instance already exists {}'.format(str(app)))
     window = MainWindow(devicewrapper=devicewrapper)
-    
+
     window.show()
-    app.exec_()    
+    app.exec_()
 
 
 # ===========================================================================
 # run main
-# ===========================================================================    
-    
+# ===========================================================================
+
 if __name__ == "__main__":
     dd = DummyDevice()
     dw = DeviceWrapper(dd)
-    
+
     main(dw)
