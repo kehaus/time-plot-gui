1e25c603e7b809bb167009b35b0ad0583ff6c3dd
kehaus <tv_junkie_4ever@hotmail.com>
2020-08-29 15:23:55 -0400

    clean up ContextMenu
    Detailed:
    * move the different submenu-generation code blocks to seperate functions to increase readability
    * change the remove_default_menu() function to allow for removing submenu by name and not by index as before
    * remove commented-out code in time_plot_gui.py

--- src/context_menu.py
+++ src/context_menu.py
@@ -31,135 +31,51 @@ class ContextMenu():
         self.visualization_settings = self.menu.addMenu("Visualization Settings")
         self.data_options = self.menu.addMenu("Data Options")
         self.change_labels_menu = self.menu.addMenu("Change Labels")
+
         # ===============================
         # Submenu Formation: line_settings
         # ===============================
-        self.add_line_settings_menu()
+        self._add_line_settings_menu()
+
         # ===============================
         # Submenu Formation: visualization_settings
         # ===============================
-        restore_default = QtGui.QAction("Restore Default Plot Settings", self.visualization_settings)
-        restore_default.triggered.connect(self.tpg.restore_default_settings)
-        self.visualization_settings.addAction(restore_default)
-        self.visualization_settings.restore_default = restore_default
+        self._add_visualization_settings_menu()
 
-        restore_saved = QtGui.QAction("Restore Saved Plot Settings", self.visualization_settings)
-        restore_saved.triggered.connect(self.tpg.set_custom_settings)
-        self.visualization_settings.addAction(restore_saved)
-        self.visualization_settings.restore_saved = restore_saved
-
-        save_settings = QtGui.QAction("Save Current Plot Settings", self.visualization_settings)
-        save_settings.triggered.connect(self.tpg.save_current_settings)
-        self.visualization_settings.addAction(save_settings)
-        self.visualization_settings.save_settings = save_settings
-
-        clear_line_settings = QtGui.QAction("Clear Line Settings", self.visualization_settings)
-        clear_line_settings.triggered.connect(self.tpg.clear_line_settings)
-        self.visualization_settings.addAction(clear_line_settings)
-        self.visualization_settings.clear_line_settings = clear_line_settings
         # ===============================
         # Submenu Formation: Data Options
         # ===============================
-        clear_data = QtGui.QAction("Clear Data", self.data_options)
-        clear_data.triggered.connect(self.tpg.clear_all_data)
-        self.data_options.addAction(clear_data)
-        self.data_options.clear_data = clear_data
-
-        automatic_clear = QtGui.QWidgetAction(self.data_options)
-        automatic_clear_checkbox = QtGui.QCheckBox("Clear Old Data on Start")
-        automatic_clear.setDefaultWidget(automatic_clear_checkbox)
-        automatic_clear_checkbox.stateChanged.connect(self.tpg.save_data_settings)
-        self.data_options.addAction(automatic_clear)
-        self.data_options.automatic_clear = automatic_clear
-        self.data_options.automatic_clear_checkbox = automatic_clear_checkbox
-
-        autosave = QtGui.QWidgetAction(self.data_options)
-        autosave_widget = QWidget()
-        autosave_layout = QHBoxLayout()
-        autosave_layout.setContentsMargins(0,0,0,0)
-        autosave_checkbox = QtGui.QCheckBox("Automatically Save Data")
-        autosave_checkbox.stateChanged.connect(self.tpg.set_all_autosave)
-        autosave_checkbox.setChecked(self.tpg.settings['do_autosave'])
-        autosave_nr = QSpinBox()
-        autosave_nr.setButtonSymbols(QAbstractSpinBox().NoButtons)
-        autosave_nr.setRange(10, 1000)
-        autosave_nr.setValue(self.tpg.settings['autosave_nr'])
-        # autosave_nr.setSingleStep(10)
-        autosave_nr.valueChanged.connect(self.tpg.set_all_autosave_nr)
-        autosave_layout.addWidget(autosave_checkbox)
-        autosave_layout.addWidget(autosave_nr)
-        autosave_widget.setLayout(autosave_layout)
-        autosave.setDefaultWidget(autosave_widget)
-        self.data_options.addAction(autosave)
-        self.data_options.autosave = autosave
+        self._add_data_option_menu()
 
         # ===============================
         # Submenu Formation: Change Labels
         # ===============================
-        change_title = QtGui.QAction("Change Plot Title", self.change_labels_menu)
-        change_title.triggered.connect(self.tpg.change_title)
-        self.change_labels_menu.addAction(change_title)
-        self.change_labels_menu.change_title = change_title
+        self._add_change_label_menu()
 
-        change_x_axis_label = QtGui.QAction("Change X Axis Label", self.change_labels_menu)
-        change_x_axis_label.triggered.connect(self.tpg.change_x_axis_label)
-        self.change_labels_menu.addAction(change_x_axis_label)
-        self.change_labels_menu.change_x_axis_label = change_x_axis_label
-
-        change_y_axis_label = QtGui.QAction("Change Y Axis Label", self.change_labels_menu)
-        change_y_axis_label.triggered.connect(self.tpg.change_y_axis_label)
-        self.change_labels_menu.addAction(change_y_axis_label)
-        self.change_labels_menu.change_y_axis_label = change_y_axis_label
-
-        relative_time = QtGui.QWidgetAction(self.change_labels_menu)
-        relative_time_checkbox = QtGui.QCheckBox("Relative Time Markers")
-        relative_time.setDefaultWidget(relative_time_checkbox)
-        relative_time_checkbox.setChecked(self.tpg.settings['relative_timestamp'])
-        relative_time_checkbox.stateChanged.connect(self.tpg.change_time_markers)
-        self.change_labels_menu.addAction(relative_time)
-        self.change_labels_menu.relative_time = relative_time
-        self.change_labels_menu.relative_time_checkbox = relative_time_checkbox
         # ===============================
         # Function Formation: Load Past Data
         # ===============================
-        open_data = QtGui.QAction("Load Stored Data")
-        open_data.triggered.connect(self.tpg.open_finder)
-        self.menu.addAction(open_data)
-        self.menu.open_data = open_data
+        self._add_load_past_data()
+        
         # ===============================
         # Submenu revision: local fourier transform
         # ===============================
-        self.transform_menu = self.menu.actions()[0].menu()
-        self.transform_menu.actions()[0].defaultWidget().layout().setContentsMargins(10,10,10,0)
-
-        self.x_log_check = self.transform_menu.actions()[0].defaultWidget().layout().itemAt(1).widget()
-        self.y_log_check = self.transform_menu.actions()[0].defaultWidget().layout().itemAt(2).widget()
-
-        local_fourier = QtGui.QWidgetAction(self.transform_menu)
-        local_fourier_widget = QWidget()
-        lf_label = QLabel("Local Fourier Mode")
-        local_fourier_checkbox = QtGui.QCheckBox()
-        local_fourier_checkbox.stateChanged.connect(self.tpg.set_local_ft_mode)
-        lf_layout = QHBoxLayout()
-        lf_layout.setContentsMargins(10,0,0,0)
-        lf_layout.addWidget(lf_label)
-        lf_layout.addWidget(local_fourier_checkbox)
-        local_fourier_widget.setLayout(lf_layout)
-        local_fourier.setDefaultWidget(local_fourier_widget)
-        self.transform_menu.addAction(local_fourier)
-        self.transform_menu.local_fourier = local_fourier
+        self._add_local_fft()
 
         # ===============================
         # Remove unnecesary default context menu operations
         # ===============================
-        actions = self.tpg.graphItem.ctrlMenu.actions()
-        #1,2,3,5
-        for index in range(len(actions)):
-            self.tpg.graphItem.ctrlMenu.removeAction(actions[index])
-        for index in [0, 4, 6, 7, 8, 9, 10]:
-            self.tpg.graphItem.ctrlMenu.addAction(actions[index])
+        self.remove_options_from_default_contextmenu()
 
-    def add_line_settings_menu(self):
+
+    def _add_line_settings_menu(self):
+        """add line setting submenu to Plot Options menu
+        
+        Line settings menu consists of multiple blocks such that every data
+        line present in the plot has a corresponding block. One block consists 
+        consists of QWidgets to change line color, alpha value, and width.
+        
+        """
         # ===============================
         # remove existing items from the menu
         # ===============================
@@ -210,10 +126,195 @@ class ContextMenu():
             self.line_settings_menu.addAction(change_line_color)
             self.line_settings_menu.change_line_color = change_line_color
 
+    def _add_visualization_settings_menu(self):
+        """add visualization submenu to Plot Options menu
+        
+        Visualization menu contains actions to load, save, clear, and restore 
+        plot settings.
+        
+        """
+        restore_default = QtGui.QAction(
+            "Restore Default Plot Settings", 
+            self.visualization_settings
+        )
+        restore_default.triggered.connect(self.tpg.restore_default_settings)
+        self.visualization_settings.addAction(restore_default)
+        self.visualization_settings.restore_default = restore_default
+
+        restore_saved = QtGui.QAction(
+            "Restore Saved Plot Settings", 
+            self.visualization_settings
+        )
+        restore_saved.triggered.connect(self.tpg.set_custom_settings)
+        self.visualization_settings.addAction(restore_saved)
+        self.visualization_settings.restore_saved = restore_saved
+
+        save_settings = QtGui.QAction(
+            "Save Current Plot Settings", 
+            self.visualization_settings
+        )
+        save_settings.triggered.connect(self.tpg.save_current_settings)
+        self.visualization_settings.addAction(save_settings)
+        self.visualization_settings.save_settings = save_settings
+
+        clear_line_settings = QtGui.QAction(
+            "Clear Line Settings", 
+            self.visualization_settings
+        )
+        clear_line_settings.triggered.connect(self.tpg.clear_line_settings)
+        self.visualization_settings.addAction(clear_line_settings)
+        self.visualization_settings.clear_line_settings = clear_line_settings
+        
+    def _add_data_option_menu(self):
+        """add data option submenu to Plot Options menu
+        
+        Data option menu contains actions to to clear present data; set the
+        clear-on-startup option; and enable data autosave option
+        
+        """
+        clear_data = QtGui.QAction(
+            "Clear Data", 
+            self.data_options
+        )
+        clear_data.triggered.connect(self.tpg.clear_all_data)
+        self.data_options.addAction(clear_data)
+        self.data_options.clear_data = clear_data
+
+        automatic_clear = QtGui.QWidgetAction(self.data_options)
+        automatic_clear_checkbox = QtGui.QCheckBox("Clear Old Data on Start")
+        automatic_clear.setDefaultWidget(automatic_clear_checkbox)
+        automatic_clear_checkbox.stateChanged.connect(self.tpg.save_data_settings)
+        self.data_options.addAction(automatic_clear)
+        self.data_options.automatic_clear = automatic_clear
+        self.data_options.automatic_clear_checkbox = automatic_clear_checkbox
+
+        autosave = QtGui.QWidgetAction(self.data_options)
+        autosave_widget = QWidget()
+        autosave_layout = QHBoxLayout()
+        autosave_layout.setContentsMargins(0,0,0,0)
+        autosave_checkbox = QtGui.QCheckBox("Automatically Save Data")
+        autosave_checkbox.stateChanged.connect(self.tpg.set_all_autosave)
+        autosave_checkbox.setChecked(self.tpg.settings['do_autosave'])
+        autosave_nr = QSpinBox()
+        autosave_nr.setButtonSymbols(QAbstractSpinBox().NoButtons)
+        autosave_nr.setRange(10, 1000)
+        autosave_nr.setValue(self.tpg.settings['autosave_nr'])
+        # autosave_nr.setSingleStep(10)
+        autosave_nr.valueChanged.connect(self.tpg.set_all_autosave_nr)
+        autosave_layout.addWidget(autosave_checkbox)
+        autosave_layout.addWidget(autosave_nr)
+        autosave_widget.setLayout(autosave_layout)
+        autosave.setDefaultWidget(autosave_widget)
+        self.data_options.addAction(autosave)
+        self.data_options.autosave = autosave        
+
+    def _add_change_label_menu(self):
+        """add change label submenu to Plot Option menu
+        
+        Chane label menu contians functions to change title, and axes labels. 
+        Furthermore it allows to change time scale from relative time values to
+        absolute timve values
+        
+        """
+        change_title = QtGui.QAction(
+            "Change Plot Title", 
+            self.change_labels_menu
+        )
+        change_title.triggered.connect(self.tpg.change_title)
+        self.change_labels_menu.addAction(change_title)
+        self.change_labels_menu.change_title = change_title
+
+        change_x_axis_label = QtGui.QAction(
+            "Change X Axis Label", 
+            self.change_labels_menu
+        )
+        change_x_axis_label.triggered.connect(self.tpg.change_x_axis_label)
+        self.change_labels_menu.addAction(change_x_axis_label)
+        self.change_labels_menu.change_x_axis_label = change_x_axis_label
+
+        change_y_axis_label = QtGui.QAction(
+            "Change Y Axis Label", 
+            self.change_labels_menu
+        )
+        change_y_axis_label.triggered.connect(self.tpg.change_y_axis_label)
+        self.change_labels_menu.addAction(change_y_axis_label)
+        self.change_labels_menu.change_y_axis_label = change_y_axis_label
+
+        relative_time = QtGui.QWidgetAction(self.change_labels_menu)
+        relative_time_checkbox = QtGui.QCheckBox("Relative Time Markers")
+        relative_time.setDefaultWidget(relative_time_checkbox)
+        relative_time_checkbox.setChecked(self.tpg.settings['relative_timestamp'])
+        relative_time_checkbox.stateChanged.connect(self.tpg.change_time_markers)
+        self.change_labels_menu.addAction(relative_time)
+        self.change_labels_menu.relative_time = relative_time
+        self.change_labels_menu.relative_time_checkbox = relative_time_checkbox
+
+    def _add_load_past_data(self):
+        """add load-past-data function to Plot Option menu
+        
+        This function opens a seperate finder window from where plot settings 
+        and data files can be selected to be loaded into the plot
+        
+        """
+        open_data = QtGui.QAction("Load Stored Data")
+        open_data.triggered.connect(self.tpg.open_finder)
+        self.menu.addAction(open_data)
+        self.menu.open_data = open_data
+        
+    def _add_local_fft(self):
+        """add local_fft option to the Transform submenu in the Plot Option menu
+        
+        This function ammends functionality to the Transform submenu. Since the 
+        Transform submenu is defined in the pyqtgraph ContextMenu class this
+        function has to retrieve the relevant QAction to successfully ammend 
+        the functionality.
+        
+        """
+        self.transform_menu = self.menu.actions()[0].menu()
+        self.transform_menu.actions()[0].defaultWidget().layout().setContentsMargins(10,10,10,0)
+
+        self.x_log_check = self.transform_menu.actions()[0].defaultWidget().layout().itemAt(1).widget()
+        self.y_log_check = self.transform_menu.actions()[0].defaultWidget().layout().itemAt(2).widget()
+
+        local_fourier = QtGui.QWidgetAction(self.transform_menu)
+        local_fourier_widget = QWidget()
+        lf_label = QLabel("Local Fourier Mode")
+        local_fourier_checkbox = QtGui.QCheckBox()
+        local_fourier_checkbox.stateChanged.connect(self.tpg.set_local_ft_mode)
+        lf_layout = QHBoxLayout()
+        lf_layout.setContentsMargins(10,0,0,0)
+        lf_layout.addWidget(lf_label)
+        lf_layout.addWidget(local_fourier_checkbox)
+        local_fourier_widget.setLayout(lf_layout)
+        local_fourier.setDefaultWidget(local_fourier_widget)
+        self.transform_menu.addAction(local_fourier)
+        self.transform_menu.local_fourier = local_fourier
+
+    def remove_options_from_default_contextmenu(self):
+        """
+        remove some default options from the contextmenu
+        
+        Since this context menu is build upon the pyqtgraph context menu, some 
+        of the submenues are not relevant here. This function removes them.
+        
+        """
+        remove_menu_lst = ['Downsample','Average','Alpha', 'Points']
+        
+        actions = self.tpg.graphItem.ctrlMenu.actions()
+        for action in actions:
+            menu_name = action.iconText() 
+            if menu_name in remove_menu_lst:
+                self.tpg.graphItem.ctrlMenu.removeAction(action)
+        
+
     def ammend_context_menu(self):
         line_controls = self.line_settings_menu.actions()[0::2]
         key = 0
         for line in line_controls:
-            line.defaultWidget().layout().itemAt(2).widget().setValue(255*self.tpg.settings['line_settings'][str(key)]['line_alpha'])
-            line.defaultWidget().layout().itemAt(4).widget().setValue(self.tpg.settings['line_settings'][str(key)]['line_width'])
+            line.defaultWidget().layout().itemAt(2).widget().setValue(
+                255*self.tpg.settings['line_settings'][str(key)]['line_alpha']
+            )
+            line.defaultWidget().layout().itemAt(4).widget().setValue(
+                self.tpg.settings['line_settings'][str(key)]['line_width']
+            )
             key += 1
