a0239a01c557c239e77dd24a0b543ee6a6ff538d
kehaus <tv_junkie_4ever@hotmail.com>
2020-09-10 20:07:06 -0400

    add performance test scripts. not complete yet

---
+++ src/test/performance/data_update_performance.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Sep  4 06:25:36 2020
+
+@author: kh
+"""
+import os
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+from .test import test1
+
---
+++ src/test/test1.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+"""
+
+
+print('was in test1')
