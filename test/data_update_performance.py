46252e240bcc06bf5d5ce0d17a88ec4b5de2affe
kehaus <tv_junkie_4ever@hotmail.com>
2020-09-11 19:06:29 -0400

    relocate test folder

---
+++ test/data_update_performance.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Sep  4 06:25:36 2020
+
+@author: kh
+"""
+import os
+import sys
+
+import numpy as np
+import matplotlib.pyplot as plt
+from PyQt5.QtWidgets import QApplication
+
+test_mode = True
+if not test_mode:
+    from TimePlotGui import TimePlotGui, DeviceWrapper, DummyDevice
+else:
+    module_path = os.path.dirname(os.getcwd())
+    if module_path not in sys.path:
+        sys.path.append(module_path)
+    from src import TimePlotMainWindow, DeviceWrapper, DummyDevice
+
+
+
+# ===========================================================================
+# Run Gui 
+# ===========================================================================
+dd1 = DummyDevice(
+    signal_form='random', xmin=0, xmax=2,
+    frequency=5
+)
+dw1 = DeviceWrapper(dd1)
+
+dd2 = DummyDevice(
+    frequency=0.6
+)
+dw2 = DeviceWrapper(dd2)
+
+dd3 = DummyDevice(
+    signal_form='sin',
+    frequency=1.3
+)
+dw3 = DeviceWrapper(dd3)
+
+
+# ============================================================================
+# Start TimePlotGui application
+# ============================================================================
+app = QApplication.instance()
+if app is None:
+    app = QApplication(sys.argv)
+else:
+    print('QApplication instance already exists {}'.format(str(app)))
+window = TimePlotMainWindow(devices=[dw1, dw2])
+try:
+    window.show()
+    app.exec_()
+except:
+    window.closeEvent()
+
+
+# ===========================================================================
+# investigate data
+# ===========================================================================
+
